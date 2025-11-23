d3226cc95fe4bbc35594df287b0bc6924693ec19
robmf1966 <56730581+robmf1966@users.noreply.github.com>
2025-11-23 19:35:33 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Standard root base
+  optimizeDeps: { 
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/firestore',
+    ],
+  },
+});
